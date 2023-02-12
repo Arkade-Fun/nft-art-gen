c582949038f49c64aafc5e702e08f0d909302d64
Andrew <galaxysmail@protonmail.com>
2023-02-12 10:47:45 -0600

    working on 1:1 script

---
+++ utils/createOneOfOnes.js
@@ -0,0 +1,37 @@
+const OneOfOnes = require("../src/oneOfOnes.json");
+const predeterminedGen = require("./predeterminedGen.js");
+const buildDir = `${basePath}/build`;
+
+console.log("OneOfOnes =>", OneOfOnes);
+
+OneOfOnes.forEach((item) => {
+  //todo: create image
+});
+
+const saveImage = (_editionCount) => {
+  fs.writeFileSync(
+    `${buildDir}/images/${_editionCount}.png`,
+    canvas.toBuffer("image/png")
+  );
+};
+
+const addAttributes = (_element) => {
+  let selectedElement = _element.layer.selectedElement;
+  attributesList.push({
+    trait_type: _element.layer.name,
+    value: selectedElement.name,
+  });
+};
+
+const saveMetaDataSingleFile = (_editionCount) => {
+  let metadata = metadataList.find((meta) => meta.edition == _editionCount);
+  debugLogs
+    ? console.log(
+        `Writing metadata for ${_editionCount}: ${JSON.stringify(metadata)}`
+      )
+    : null;
+  fs.writeFileSync(
+    `${buildDir}/json/${_editionCount}.json`,
+    JSON.stringify(metadata, null, 2)
+  );
+};
